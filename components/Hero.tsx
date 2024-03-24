2e0ea19d31fa5e529beea9a89868d97b7c1960b2
Rzvone <bobonea.razvan.ctin@gmail.com>
2024-03-24 11:00:28 +0200

    added a light

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Lamp } from "./ui/lamp";
+
+const Hero = () => {
+  return (
+    <div className="w-full">
+      <Lamp />
+    </div>
+  );
+};
+
+export default Hero;
